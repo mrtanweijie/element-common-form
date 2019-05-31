aa286949f5dff58bb79ccb4b3fdc8e864f3cd6a5
weijietan <weijietan@lexinfintech.com>
2019-05-31 16:54:18 +0800

    . 文件

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/strongly-recommended',
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    // vue规则 - tag自动闭合不强制
+    'vue/html-self-closing': 'off',
+    'vue/max-attributes-per-line': 'off',
+    
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+}
